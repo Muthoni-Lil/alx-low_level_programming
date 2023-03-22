54a3ff7255480d4fc1cfaf86f47cfad2648567f7
Muthoni-Lil <naserianmuthoni@gmail.com>
2023-03-22 13:41:03 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,14 +18,14 @@ void print_times_table(int n)
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
 	{
-	_putchar('_');
+	_putchar(',');
 	_putchar(' ');
 	prod = num * mult;
 	if (prod <= 99)
 	_putchar(' ');
 	if (prod <= 9)
 	_putchar(' ');
-	if (prod <= 100)
+	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
 	_putchar(((prod / 10)) % 10 + '0');
